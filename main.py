95ad8f7792199b5578570f882e9069569fe4e6c9
Zinmarlwin711 <zinzin9546@gmail.com>
2018-07-16 18:42:40 -1200

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template.url_for
+from flask import Flask,render_template, url_for
 
 myapp = Flask(__name__)
 @myapp.route("/")
@@ -8,5 +8,5 @@ def hello():
 @myapp.route("/compose")
 def compose():
     return render_template("compose.html")
-if __name__="__main__":
+if __name__=="__main__":
     myapp.run(debug=True)
